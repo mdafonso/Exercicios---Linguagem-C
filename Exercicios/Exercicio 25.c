bd0b083c12721b32bc17350ff2f2ea9ff2c10b15
Mariana Duarte Afonso <85906812+mdafonso@users.noreply.github.com>
2023-03-30 09:07:55 -0300

    Create Exercicio 25.c

---
+++ Exercicios/Exercicio 25.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(void) {
+  float reta1,reta2,reta3;
+  printf("\nQual o valor da reta 1 ? ");
+  scanf("%f", &reta1);
+  printf("\nQual o valor da reta 2 ? ");
+  scanf("%f", &reta2);
+  printf("\nQual o valor da reta 3 ? ");
+  scanf("%f", &reta3);
+  if (reta1<reta2+reta3&&reta2<reta1+reta3&&reta3<reta1+reta2)
+    
+    printf("\n----- É possivel forma um triângulo -----");
+
+  else  
+    printf("\n----- Não é possivel forma um triângulo -----");
+    
+  
+
+  
+  return 0;
+}
+
+    //OUTRA FORMA MAS EXPECIFICA DE TRIÂNGULO
+   
+    int a, b, c;
+
+    printf("Digite tres valores: ");
+    scanf("%d%d%d", &a, &b, &c);
+
+    if(a + b > c && a + c > b && b + c > a){
+        printf("Os 3 lados formam um triangulo!\n");
+        if(a == b && a == c)
+            printf("Equilatero\n");
+        else
+            if(a == b || a == c || b == c)
+                printf("Isosceles\n");
+            else
+                printf("Escaleno\n");
+    }
+    else
+        printf("Os 3 lados NAO formam um trinagulo!\n");
+}//
