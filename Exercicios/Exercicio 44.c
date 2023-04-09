7a164148246aaccdc6d0a9249e2cdb4e675c309f
Mariana Duarte Afonso <85906812+mdafonso@users.noreply.github.com>
2023-04-09 10:40:46 -0300

    Create Exercicio 44.c

---
+++ Exercicios/Exercicio 44.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+  int valori, valorf,in;
+  
+  printf("\nDigite o primeiro valor: ");
+  scanf("%d",&valori);
+  printf("\nDigite o último valor: ");
+  scanf("%d",&valorf);
+  printf("\nDigite o incremento: ");
+  scanf("%d",&in);
+
+  while (valori<valorf)
+  {  
+     printf("\n %i",valori);
+     valori=valori+in;
+  }
+    
+  return 0;
+}
+
+
+         
