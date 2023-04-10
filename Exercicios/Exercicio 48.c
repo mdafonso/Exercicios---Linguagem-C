8915254b67e7316e370634fd7dd49fcd495f2e5c
Mariana Duarte Afonso <85906812+mdafonso@users.noreply.github.com>
2023-04-10 13:20:36 -0300

    Create Exercicio 48.c

---
+++ Exercicios/Exercicio 48.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void) {
+  
+  int n1,n2,n3,n4,n5,n6,n7,soma;
+
+  printf("\nEscreva um número inteiro: ");
+  scanf("%d",&n1);
+  printf("\nEscreva outro número inteiro: ");
+  scanf("%d",&n2);
+  printf("\nEscreva outro número inteiro: ");
+  scanf("%d",&n3);
+  printf("\nEscreva outro número inteiro: ");
+  scanf("%d",&n4);
+  printf("\nEscreva outro número inteiro: ");
+  scanf("%d",&n5);
+  printf("\nEscreva outro número inteiro: ");
+  scanf("%d",&n6);
+  printf("\nEscreva outro número inteiro: ");
+  scanf("%d",&n7);
+
+  
+  soma=n1+n2+n3+n4+n5+n6+n7;
+  printf("\nSOMA = %d",soma); 
+    
+
+
+
+
+
+  
+  return 0;
+}
