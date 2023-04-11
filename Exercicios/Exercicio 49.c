02beac63be1eac1203c66b1852edef130a1e120d
Mariana Duarte Afonso <85906812+mdafonso@users.noreply.github.com>
2023-04-11 12:36:43 -0300

    Create Exercicio 49.c

---
+++ Exercicios/Exercicio 49.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+  
+ int num,i,pares=0,impares=0;
+  printf("\n digite 6 números inteiros: ");
+    scanf("%d",&num);
+
+  for(i=1;i<=6;i++)
+    {
+    
+    if(num%2==0)
+    pares++;
+    else
+    impares++;
+    }
+   printf("\n Quantidade de numeros pares: %d ",pares);
+   printf("\n Quantidade de numeros impares: %d ",impares);
+  
+  
+  
+  
+  return 0;
+}
