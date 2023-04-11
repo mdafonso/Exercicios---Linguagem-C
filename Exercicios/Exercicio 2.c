c084dc6a240b493371bc07dadaf3bb0359ffe1d9
Mariana Duarte Afonso <85906812+mdafonso@users.noreply.github.com>
2023-04-11 12:38:19 -0300

    Update Exercicio 2.c

--- Exercicios/Exercicio 2.c
+++ Exercicios/Exercicio 2.c
@@ -1,3 +1,4 @@
+//Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boasvindas para ela:Ex:Qual é o seu nome? João da SilvaOlá João da Silva, é um prazer te conhecer!
 #include <stdio.h>
 
 int main(void) 
