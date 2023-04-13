5e672c1174f0bab8229e3b828d2e20a6d4c6a93b
Mariana Duarte Afonso <85906812+mdafonso@users.noreply.github.com>
2023-04-12 23:08:27 -0300

    Update Exercicio 9.c

--- Exercicios/Exercicio 9.c
+++ Exercicios/Exercicio 9.c
@@ -1,3 +1,4 @@
+//Faça um algoritmo que leia quanto dinheiro uma pessoa tem na carteira (em R$) e mostre quantos dólares ela pode comprar. Considere US$1,00 = R$3,45.
 #include <stdio.h>
 
 int main(void) {
