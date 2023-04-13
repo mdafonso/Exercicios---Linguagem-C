409f05983fc5b2239dbf6a20e5b906d8224fb479
Mariana Duarte Afonso <85906812+mdafonso@users.noreply.github.com>
2023-04-12 23:16:47 -0300

    Update Exercicio 24.c

--- Exercicios/Exercicio 24.c
+++ Exercicios/Exercicio 24.c
@@ -1,3 +1,4 @@
+//Faça um algoritmo que pergunte a distância que um passageiro deseja percorrer em Km. Calcule o preço da passagem, cobrando R$0.50 por Km para viagens até 200Km e R$0.45 para viagens mais longas.
 #include <stdio.h>
 
 int main(void) {
