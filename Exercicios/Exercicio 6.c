e2bd3c5a1170499552f82b34485a036690b9d036
Mariana Duarte Afonso <85906812+mdafonso@users.noreply.github.com>
2023-04-12 23:05:14 -0300

    Update Exercicio 6.c

--- Exercicios/Exercicio 6.c
+++ Exercicios/Exercicio 6.c
@@ -1,3 +1,4 @@
+//Faça um programa que leia um número inteiro e mostre o seu antecessor e seu sucessor.Ex:Digite um número: 9,O antecessor de 9 é 8,O sucessor de 9 é 10
 #include <stdio.h>
 #include <stdlib.h>>
 
