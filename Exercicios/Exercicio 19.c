1e8a27bcfbe9ed33a8a1484c911298e424173c41
Mariana Duarte Afonso <85906812+mdafonso@users.noreply.github.com>
2023-04-12 23:13:42 -0300

    Update Exercicio 19.c

--- Exercicios/Exercicio 19.c
+++ Exercicios/Exercicio 19.c
@@ -1,3 +1,4 @@
+//Crie um algoritmo que leia o nome e as duas notas de um aluno, calcule a sua média e mostre na tela. No final, analise a média e mostre se o aluno teve ou não um bom aproveitamento (se ficou acima da média 7.0).
 #include <stdio.h>
 #include <string.h>
 
