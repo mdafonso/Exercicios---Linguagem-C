0bcd0ee01b0e49f586b5e17a4ddbae957235ce96
Mariana Duarte Afonso <85906812+mdafonso@users.noreply.github.com>
2023-04-13 09:22:53 -0300

    Update Exercicio 30.c

--- Exercicios/Exercicio 30.c
+++ Exercicios/Exercicio 30.c
@@ -1,3 +1,4 @@
+//Refaça o algoritmo 25, acrescentando o recurso de mostrar que tipo de triângulo será formado:- EQUILÁTERO: todos os lados iguais,- ISÓSCELES: dois lados iguais,- ESCALENO: todos os lados diferentes
 #include <stdio.h>
 
 int main(void) {
