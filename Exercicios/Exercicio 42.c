304292d5c1d33c00d7d8b5eeecde5e263f4862e4
Mariana Duarte Afonso <85906812+mdafonso@users.noreply.github.com>
2023-04-16 12:17:50 -0300

    Update Exercicio 42.c

--- Exercicios/Exercicio 42.c
+++ Exercicios/Exercicio 42.c
@@ -1,3 +1,4 @@
+//Faça um algoritmo que pergunte ao usuário um número inteiro e positivo qualquer e mostre uma contagem até esse valor:Ex: Digite um valor: 35, Contagem: 1 2 3 4 5 6 7 ... 33 34 35 Acabou!
 #include <stdio.h>
 
 int main(void) {
